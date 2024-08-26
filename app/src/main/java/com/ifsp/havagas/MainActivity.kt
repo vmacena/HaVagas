6c89a2e81e7424ea1b3696d3dbba8b0d2ed65b1e
Vinicius Macena <macena.v@aluno.ifsp.edu.br>
2024-08-25 22:50:23 -0300

    feat: add logic to show/hide mobile phone field based on checkbox selection

--- app/src/main/java/com/ifsp/havagas/MainActivity.kt
+++ app/src/main/java/com/ifsp/havagas/MainActivity.kt
@@ -1,14 +1,25 @@
 package com.ifsp.havagas
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.View
+import android.widget.CheckBox
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val cbAddMobile = findViewById<CheckBox>(R.id.cb_add_mobile)
+        val etMobile = findViewById<EditText>(R.id.et_mobile)
+
+        cbAddMobile.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                etMobile.visibility = View.VISIBLE
+            } else {
+                etMobile.visibility = View.GONE
+            }
+        }
     }
 }
